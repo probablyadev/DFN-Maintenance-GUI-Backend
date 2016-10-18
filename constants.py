18b40d3bda8e3e1fe06fa1f0221f715648fa4341
CPedersen3245 <campbelljip@gmail.com>
2016-10-18 13:09:46 +0800

    More commands, feedback before command execution

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 # This file stores all constants used in this GUI.
 # The major constant here are different bash commands, executed server-side.
 statuscheck = "NOT IMPLEMENTED YET"
-gpscheck = "NOT IMPLEMENTED YET"
-cameraon = "python enable_camera.py"
-cameraoff = "python disable_camera.py"
-enableharddrive = "python enable_ext-hd.py"
-disableharddrive = "python disable_ext-hd.py"
+gpscheck = "echo gG > /dev/leostick && cat /dev/leostick"
+cameraon = "python /opt/dfn-software/enable_camera.py"
+cameraoff = "python /opt/dfn-software/disable_camera.py"
+enableharddrive = "python /opt/dfn-software/enable_ext-hd.py"
+disableharddrive = "python /opt/dfn-software/disable_ext-hd.py"
 unmountharddrive = "umount /data1 && umount /data2"
 checkspace = "du /data1 && du /data2"
--- static/js/runcommands.js
+++ static/js/runcommands.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
     // Function makes an AJAX request to the server, with the clicked button's ID as input.
     jQuery(".commandbutton").click(function(){
+        var psconsole = $('#feedbackText');
+        jQuery(psconsole).append("Doing command: " + $(this).attr('id') + "\n");
         jQuery.get("/runcommand", { buttonID : $(this).attr('id') }, function(result){
                 var psconsole = $('#feedbackText');
                 jQuery(psconsole).append(result);
