a984ab7eb7343ba161746c221cc7971e7ee99c48
CPedersen3245 <campbelljip@gmail.com>
2016-12-02 14:44:44 +0800

    Added more JS commenting to make intentions clear

--- static/js/runcommands.js
+++ static/js/runcommands.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    //Useful globals
+    //HTML element variables
     var webConsole = $('#feedbackText');
     var cameraLight = $('#cameraLight');
     var gpsLight = $('#GPSLight');
@@ -8,19 +8,21 @@ $(document).ready(function () {
     var hdd1Light = $('#HDD1Light');
     var hdd2Light = $('#HDD2Light');
 
+    //Useful globals + constants
     var doingCommand = false;
     var colorMapping = {true : "#00FF00", false: "#FF0000"};
 
-    /***********************/
-    /* CODE FOR REQUESTING */
-    /***********************/
+    /***************************************************/
+    /* CODE FOR BUTTON PRESS HANDLERS, AJAX REQUESTERS */
+    /***************************************************/
+
     //Handler for turning camera on
     function cameraOnHandler() {
         if(!doingCommand) {
             doingCommand = true;
-
+            //Feedback on button press
             $(webConsole).append("Doing command: " + $(this).attr('id') + "\n");
-            //Turn camera off
+            //Request to turn camera on
             $.getJSON("/cameraon", function (result) {
                 //Set feedback text
                 addToWebConsole(result.consoleFeedback + "\n");
@@ -36,9 +38,9 @@ $(document).ready(function () {
     function cameraOffHandler() {
         if(!doingCommand) {
             doingCommand = true;
-
+            //Feedback on button press
             $(webConsole).append("Doing command: " + $(this).attr('id') + "\n");
-            //Turn camera off
+            //Request to turn camera off
             $.getJSON("/cameraoff", function (result) {
                 //Set feedback text
                 addToWebConsole(result.consoleFeedback + "\n");
@@ -54,14 +56,14 @@ $(document).ready(function () {
     function gpsCheckHandler() {
         if(!doingCommand) {
             doingCommand = true;
-
+            //Feedback on button press
             $(webConsole).append("Doing command: " + $(this).attr('id') + "\n");
-            //Turn camera off
+            //Request to check GPS status
             $.getJSON("/gpscheck", function (result) {
                 //Set feedback text
                 addToWebConsole(result.consoleFeedback + "\n");
                 //Set light colour
-                gpsLight.css("background-color", colorMapping[result.cameraStatus]);
+                gpsLight.css("background-color", colorMapping[result.gpsStatus]);
                 //Open up for other commands to be run
                 doingCommand = false;
             });
@@ -72,9 +74,9 @@ $(document).ready(function () {
     function systemStatusHandler() {
         if(!doingCommand) {
             doingCommand = true;
-
+            //Feedback on button press
             $(webConsole).append("Doing command: " + $(this).attr('id') + "\n");
-            //Turn camera off
+            //Request for system status to be checked
             $.getJSON("/systemstatus", function (result) {
                 //Set feedback text
                 addToWebConsole(result.consoleFeedback + "\n");
@@ -88,14 +90,13 @@ $(document).ready(function () {
         }
     }
 
-
     //Button click events
     $("#CameraOn").click(cameraOnHandler);
     $("#CameraOff").click(cameraOffHandler);
     $("#GPSCheck").click(gpsCheckHandler);
     $("#StatusCheck").click(systemStatusHandler);
 
-    //Code for adding to console
+    //Code for adding to web console
     function addToWebConsole(inputText) {
         $(webConsole).append(inputText);
         if (webConsole.length)
