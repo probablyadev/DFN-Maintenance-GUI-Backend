95cdbecf0b01f8ba352b3b148f321d8a166d610a
CPedersen3245 <campbelljip@gmail.com>
2017-01-05 12:43:06 +0800

    Added tab system

--- static/new-frontend-src/assets/js/functions.js
+++ static/new-frontend-src/assets/js/functions.js
@@ -1,5 +1,28 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  // Get started!
+  //Code for tab controls
+  function changeTab(event)
+  {
+    var i, tabContent, tabControl;
+    var contentName = event.data.contentName;
+    var tabName = event.data.tabName;
 
+    $(".tabContent").each(function() {
+      $(this).css("display", "none");
+    })
+
+    $(".tabControl li").each(function() {
+      $(this).removeClass("active");
+    })
+
+    $("." + contentName).css("display", "block");
+    $("#" + tabName).addClass("active");
+  }
+
+  $("#statusTab").click({tabName: "statusTab", contentName: "statusControl"}, changeTab);
+  $("#cameraTab").click({tabName: "cameraTab", contentName: "cameraControl"}, changeTab);
+  $("#hddTab").click({tabName: "hddTab", contentName: "hddControl"}, changeTab);
+  $("#networkTab").click({tabName: "networkTab", contentName: "networkControl"}, changeTab);
+  $("#gpsTab").click({tabName: "gpsTab", contentName: "gpsControl"}, changeTab);
+  $("#advancedTab").click({tabName: "advancedTab", contentName: "advancedControl"}, changeTab);
 });
