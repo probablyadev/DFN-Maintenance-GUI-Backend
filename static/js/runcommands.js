02529cc806d8ddd760709cb7a413fe71b2ed9f67
CPedersen3245 <campbelljip@gmail.com>
2017-01-19 14:22:54 +0800

    Integrated new interface:

--- static/js/runcommands.js
+++ static/js/runcommands.js
@@ -357,13 +357,13 @@ $(document).ready(function () {
             //Request file
             $.get("/statusconfig", function (result) {
                 var decoded = decode64(result)
-                  var element = document.createElement('a');
-                  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(decoded));
-                  element.setAttribute('download', 'dfnstation.cfg');
-                  element.style.display = 'none';
-                  document.body.appendChild(element);
-                  element.click();
-                  document.body.removeChild(element);
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(decoded));
+                element.setAttribute('download', 'dfnstation.cfg');
+                element.style.display = 'none';
+                document.body.appendChild(element);
+                element.click();
+                document.body.removeChild(element);
             });
         }
     }
@@ -392,4 +392,64 @@ $(document).ready(function () {
             });
         }
     }
+
+    //Change img with svg source to inline svg
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+    });
+
+    //Code for tab controls
+    function changeTab(event) {
+        var i, tabContent, tabControl;
+        var contentName = event.data.contentName;
+        var tabName = event.data.tabName;
+
+        $(".tabContent").each(function () {
+            $(this).css("display", "none");
+        });
+
+        $(".tabControl li").each(function () {
+            $(this).removeClass("active");
+        });
+
+        $("." + contentName).css("display", "flex");
+        $("#" + tabName).addClass("active");
+    }
+
+    $("#statusTab").click({tabName: "statusTab", contentName: "statusControl"}, changeTab);
+    $("#cameraTab").click({tabName: "cameraTab", contentName: "cameraControl"}, changeTab);
+    $("#hddTab").click({tabName: "hddTab", contentName: "hddControl"}, changeTab);
+    $("#networkTab").click({tabName: "networkTab", contentName: "networkControl"}, changeTab);
+    $("#gpsTab").click({tabName: "gpsTab", contentName: "gpsControl"}, changeTab);
+    $("#advancedTab").click({tabName: "advancedTab", contentName: "advancedControl"}, changeTab);
+    $("#statusTab").trigger("click");
 });
