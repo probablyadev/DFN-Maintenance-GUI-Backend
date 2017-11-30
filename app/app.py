7c677a6258b5b1fb8969662df23d190d30b578f6
ScottDay <17160182@student.curtin.edu.au>
2017-11-30 21:18:08 +1100

    feat: added backend authentication

--- app/app.py
+++ app/app.py
@@ -1,4 +1,9 @@
-app = create_app()
+from flask import render_template, request, jsonify
+
+from app.model import User
+from app.utils.auth import generate_token, verify_token
+from index import app
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -14,10 +19,11 @@ def any_root_path(path):
 def get_token():
     incoming = request.get_json()
     user = User.get_user_with_email_and_password(incoming["email"], incoming["password"])
-    if user:
-        return jsonify(token=generate_token(user))
 
-    return jsonify(error=True), 403
+    if user:
+        return jsonify(token = generate_token(user))
+    else:
+        return jsonify(error = True), 403
 
 
 @app.route("/api/is_token_valid", methods=["POST"])
