99ca7a3d67f82d9944886e5c023a13613f1acdd0
ScottDay <17160182@student.curtin.edu.au>
2017-12-04 21:34:27 +1100

    feat: updated gps api endpoint

--- app/api/gps.py
+++ app/api/gps.py
@@ -6,9 +6,9 @@ from command.gps import gps_check
 gps_endpoints = Blueprint("gps_api", __name__)
 
 
-@gps_endpoints.route("/api/gps/gps_check", methods = ["GET"])
+@gps_endpoints.route("/api/gps/check_gps", methods = ["GET"])
 @requires_auth
-def gps_check_endpoint():
+def check_gps_endpoint():
     """Delivers a summary of the GPS status."""
     gps_check_message, gps_check_status = gps_check()
 
