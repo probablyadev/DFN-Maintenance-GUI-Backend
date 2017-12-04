c77a60ae18957cbd5c9bbcf70abe50a3ab0cfd1f
ScottDay <17160182@student.curtin.edu.au>
2017-12-04 21:42:33 +1100

    feat: updated time api

--- frontend/src/utils/api/time.js
+++ frontend/src/utils/api/time.js
@@ -5,8 +5,8 @@ export function output_time(token) {
     return axios.get('/api/time/output_time', tokenConfig(token));
 }
 
-export function timezone_change(token) {
-    return axios.post('/api/time/timezone_change', 
+export function change_timezone(token) {
+    return axios.post('/api/time/change_timezone',
         {
             timezone
         },
