7a7ee4a7493b5fa07fa849599f5d8a80722924d4
ScottDay <17160182@student.curtin.edu.au>
2017-12-09 15:32:15 +1100

    fix: routes imports

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,10 +4,9 @@ import React from 'react';
 import { Route } from 'react-router';
 
 // Containers
-import { App } from './containers/App';
+import { App } from './containers/AppContainer';
 import { HomeContainer } from './containers/HomeContainer';
 import LoginView from './components/LoginView';
-import RegisterView from './components/RegisterView';
 import ProtectedView from './components/ProtectedView';
 import Analytics from './components/Analytics';
 import NotFound from './components/NotFound';
@@ -20,7 +19,6 @@ export default (
     <Route path="/" component={App}>
         <Route path="main" component={requireAuthentication(ProtectedView)} />
         <Route path="login" component={requireNoAuthentication(LoginView)} />
-        <Route path="register" component={requireNoAuthentication(RegisterView)} />
         <Route path="home" component={requireNoAuthentication(HomeContainer)} />
         <Route path="analytics" component={requireAuthentication(Analytics)} />
         <Route path="*" component={DetermineAuth(NotFound)} />
