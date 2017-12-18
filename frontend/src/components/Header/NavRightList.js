9321bad1c47c94429dd6fae09864fec6545f73b9
ScottDay <17160182@student.curtin.edu.au>
2017-12-18 22:42:41 +1100

    feat: refactored header

--- frontend/src/components/Header/NavRightList.js
+++ frontend/src/components/Header/NavRightList.js
@@ -1,61 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import AccountCircleIcon from 'material-ui/svg-icons/action/account-circle';
+import ForwardIcon from 'material-ui/svg-icons/content/forward';
 import { withRouter } from 'react-router-dom';
 
-const ImgIconButtonStyle = {
-  width: '60px',
-  height: '60px'
-};
+const StyledListItem = styled.li`
+	marginRight: 10px;
+`;
+
+const StyledMenuItem = styled(MenuItem)`
+	fontSize: 14px;
+	lineHeight: 48px;
+`;
+
+const StyledIconButton = styled(IconButton)`
+	width: 60px;
+	height: 60px;
+	paddingTop: 18px;
+`;
 
 const listItemStyle = {
-  paddingLeft: '50px' // 36 + 16, algin with sub list
+  paddingLeft: '50px' // Align with sub list.
 };
 
 class NavRightList extends React.Component {
+    handleChange = (event, value) => {
+        this.props.history.push(value);
+    };
 
-  handleChange = (event, value) => {
-    this.props.history.push(value);
-  }
-
-  render() {
-    return (
-      <ul className="list-unstyled float-right">
-        <li style={{marginRight: '10px'}}>
-          <IconMenu
-            iconButtonElement={<IconButton style={ImgIconButtonStyle}><img src="assets/images/g1.jpg" alt="" className="rounded-circle img30_30" /></IconButton>}
-            onChange={this.handleChange}
-            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            menuStyle={{minWidth: '150px'}}
+    render() {
+        return (
+            <ul className="list-unstyled float-right">
+                <StyledListItem>
+                    <IconMenu
+                        iconButtonElement={
+                            <StyledIconButton>
+                                <MoreVertIcon />
+                            </StyledIconButton>
+                        }
+                        onChange={this.handleChange}
+                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                        menuStyle={{minWidth: '150px'}}
                     >
-            <MenuItem
-              value="/app/dashboard"
-              primaryText="Dashboard"
-              style={{fontSize: '14px', lineHeight: '48px'}}
-              innerDivStyle={listItemStyle}
-              leftIcon={<i className="material-icons">home</i>}
+                        <StyledMenuItem
+                            value="/app/profile"
+                            primaryText="Profile"
+                            innerDivStyle={listItemStyle}
+                            leftIcon={<AccountCircleIcon />}
                         />
-            <MenuItem
-              value="/app/page/about"
-              primaryText="About"
-              innerDivStyle={listItemStyle}
-              style={{fontSize: '14px', lineHeight: '48px'}}
-              leftIcon={<i className="material-icons">person_outline</i>}
+                        <StyledMenuItem
+                            value="/login"
+                            primaryText="Log Out"
+                            innerDivStyle={listItemStyle}
+                            leftIcon={<ForwardIcon />}
                         />
-            <MenuItem
-              value="/login"
-              primaryText="Log Out"
-              innerDivStyle={listItemStyle}
-              style={{fontSize: '14px', lineHeight: '48px'}}
-              leftIcon={<i className="material-icons">forward</i>}
-                        />
-          </IconMenu>
-        </li>
-      </ul>
-    );
-  }
+                    </IconMenu>
+                </StyledListItem>
+            </ul>
+        );
+    }
 }
 
 module.exports = withRouter(NavRightList);
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -3,67 +3,66 @@ import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import APPCONFIG from 'constants/Config';
-import NavLeftList from './NavLeftList';
 import NavRightList from './NavRightList';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 
 
 class Header extends React.Component {
-  componentDidMount() {
-    const sidebarToggler = this.sidebarBtn;
-    const $sidebarToggler = $(sidebarToggler);
-    const $body = $('#body');
+    componentDidMount() {
+        const sidebarToggler = this.sidebarBtn;
+        const $sidebarToggler = $(sidebarToggler);
+        const $body = $('#body');
 
-    $sidebarToggler.on('click', (e) => {
-      // _sidebar.scss, _page-container.scss
-      $body.toggleClass('sidebar-mobile-open');
-    });
-  }
+        $sidebarToggler.on('click', (e) => {
+            // _sidebar.scss, _page-container.scss
+            $body.toggleClass('sidebar-mobile-open');
+        });
+    }
 
-  render() {
-    const { isFixedHeader, colorOption } = this.props;
+    render() {
+        const { isFixedHeader, colorOption } = this.props;
 
-    return (
-      <section className="app-header">
-        <div
-          className={classnames('app-header-inner', {
-            'bg-color-light': ['11', '12', '13', '14', '15', '16', '21'].indexOf(colorOption) >= 0,
-            'bg-color-dark': colorOption === '31',
-            'bg-color-primary': ['22', '32'].indexOf(colorOption) >= 0,
-            'bg-color-success': ['23', '33'].indexOf(colorOption) >= 0,
-            'bg-color-info': ['24', '34'].indexOf(colorOption) >= 0,
-            'bg-color-warning': ['25', '35'].indexOf(colorOption) >= 0,
-            'bg-color-danger': ['26', '36'].indexOf(colorOption) >= 0 })}
+        return (
+            <section className="app-header">
+                <div
+                    className={classnames('app-header-inner', {
+                        'bg-color-light': ['11', '12', '13', '14', '15', '16', '21'].indexOf(colorOption) >= 0,
+                        'bg-color-dark': colorOption === '31',
+                        'bg-color-primary': ['22', '32'].indexOf(colorOption) >= 0,
+                        'bg-color-success': ['23', '33'].indexOf(colorOption) >= 0,
+                        'bg-color-info': ['24', '34'].indexOf(colorOption) >= 0,
+                        'bg-color-warning': ['25', '35'].indexOf(colorOption) >= 0,
+                        'bg-color-danger': ['26', '36'].indexOf(colorOption) >= 0
+                    })}
                 >
-          <div className="d-lg-none d-xl-none float-left">
-            <a href="javascript:;" className="md-button header-icon toggle-sidebar-btn" ref={(c) => { this.sidebarBtn = c; }}>
-              <i className="material-icons">menu</i>
-            </a>
-          </div>
+                    <div className="d-lg-none d-xl-none float-left">
+                        <a href="javascript:;" className="md-button header-icon toggle-sidebar-btn" ref={(c) => { this.sidebarBtn = c; }}>
+                            <MenuIcon />
+                        </a>
+                    </div>
 
-          <div className="brand d-none d-lg-inline-block d-xl-inline-block">
-            <h2><Link to="/">{APPCONFIG.brand}</Link></h2>
-          </div>
+                    <div className="brand d-none d-lg-inline-block d-xl-inline-block">
+                        <h2>
+                            <Link to="/">{APPCONFIG.brand}</Link>
+                        </h2>
+                    </div>
 
-          <div className="top-nav-left d-none d-lg-inline-block d-xl-inline-block">
-            <NavLeftList />
-          </div>
-
-          <div className="top-nav-right">
-            <NavRightList />
-          </div>
-        </div>
-      </section>
-    );
-  }
+                    <div className="top-nav-right">
+                        <NavRightList />
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 
 const mapStateToProps = state => ({
-  colorOption: state.settings.colorOption,
-  isFixedHeader: state.settings.isFixedHeader
+    colorOption: state.settings.colorOption,
+    isFixedHeader: state.settings.isFixedHeader
 });
 
 module.exports = connect(
-  mapStateToProps
+    mapStateToProps
 )(Header);
 
