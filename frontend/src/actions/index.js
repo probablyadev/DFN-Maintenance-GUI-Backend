0a4b2392bfd04c1c56898fa0c22d17a7dea177dc
ScottDay <17160182@student.curtin.edu.au>
2017-12-27 07:41:37 +0800

    fix: removed unused actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,16 +1,7 @@
 import * as types from '../constants/ActionTypes';
 
-export function toggleBoxedLayout(isLayoutBoxed) {
-  return { type: types.TOGGLE_BOXED_LAYOUT, isLayoutBoxed };
-}
 export function toggleCollapsedNav(isNavCollapsed) {
   return { type: types.TOGGLE_COLLAPSED_NAV, isNavCollapsed };
-}
-export function toggleNavBehind(isNavBehind) {
-  return { type: types.TOGGLE_NAV_BEHIND, isNavBehind };
-}
-export function toggleFixedHeader(isFixedHeader) {
-  return { type: types.TOGGLE_FIXED_HEADER, isFixedHeader };
 }
 export function changeSidebarWidth(sidebarWidth) {
   return { type: types.CHANGE_SIDEBAR_WIDTH, sidebarWidth };
