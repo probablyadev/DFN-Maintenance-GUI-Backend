f5a0b041bdbf4cd035e26f808f01b88fa29d55e6
ScottDay <17160182@student.curtin.edu.au>
2018-02-23 12:41:55 +1000

    fix: location sizing

--- frontend/src/routes/app/routes/location/components/Map/Map.js
+++ frontend/src/routes/app/routes/location/components/Map/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LocationMap from './LocationMap';
-import TimeZoneMap from './TimeZoneMap';
+import TimeZonePicker from './TimeZonePicker';
 
 class Map extends React.Component {
     render() {
@@ -8,17 +8,17 @@ class Map extends React.Component {
             <article className="article">
                 <h2 className="article-title">Maps</h2>
                 <div className="row">
-                    <div className="col-xl-6">
+                    <div className="col-xl-3">
                         <div className="box box-default">
                             <div className="box-body">
-                                <LocationMap/>
+                                <TimeZonePicker/>
                             </div>
                         </div>
                     </div>
-                    <div className="col-xl-6">
+                    <div className="col-xl-9">
                         <div className="box box-default">
                             <div className="box-body">
-                                <TimeZoneMap/>
+                                <LocationMap/>
                             </div>
                         </div>
                     </div>
---
+++ frontend/src/routes/app/routes/location/components/Map/TimeZonePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TimeZonePicker extends React.Component {
+    render() {
+        return (
+            <div>TimeZone Picker</div>
+        );
+    }
+}
+
+module.exports = TimeZonePicker;
