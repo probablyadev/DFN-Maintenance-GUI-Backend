f7440823dbd3310971334f54b503f9b6495570ea
ScottDay <17160182@student.curtin.edu.au>
2018-03-02 14:48:14 +0800

    feat: updated hdd status panel

--- frontend/src/routes/app/routes/storage/components/StatusPanel.js
+++ frontend/src/routes/app/routes/storage/components/StatusPanel.js
@@ -3,27 +3,24 @@ import React from 'react';
 class StatusPanel extends React.Component {
     render() {
         return (
-            <div className="box box-default no-breadcrumbs">
+            <div className="box box-default">
                 <div className="box-body">
                     <div className="row">
                         <div className="box box-transparent">
+                            <div className="box-header">Status Panel</div>
                             <div className="box-body">
                                 <div className="row text-center metrics">
-                                    <div className="col-xs-6 col-md-3 metric-box">
+                                    <div className="col-xs-4 col-md-4 metric-box">
                                         <span className="metric">4</span>
-                                        <span className="metric-info">Total Connected Drives</span>
+                                        <span className="metric-info">Hard Drives</span>
                                     </div>
-                                    <div className="col-xs-6 col-md-3 metric-box">
-                                        <span className="metric">2</span>
-                                        <span className="metric-info">Offline Drives</span>
+                                    <div className="col-xs-4 col-md-4 metric-box">
+                                        <span className="metric">5GB</span>
+                                        <span className="metric-info">Total Space</span>
                                     </div>
-                                    <div className="col-xs-6 col-md-3 metric-box">
-                                        <span className="metric">1</span>
-                                        <span className="metric-info">Powered On Drive</span>
-                                    </div>
-                                    <div className="col-xs-6 col-md-3 metric-box">
-                                        <span className="metric">1</span>
-                                        <span className="metric-info">Mounted Drive</span>
+                                    <div className="col-xs-4 col-md-4 metric-box">
+                                        <span className="metric">3GB</span>
+                                        <span className="metric-info">Used Space</span>
                                     </div>
                                 </div>
                             </div>
