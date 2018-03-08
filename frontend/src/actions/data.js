068ed52092cb24b6b4a2d7bb69ead3e2564d1fb4
ScottDay <17160182@student.curtin.edu.au>
2018-03-08 12:44:09 +0800

    feat: added data action and constants

---
+++ frontend/src/actions/data.js
@@ -0,0 +1,36 @@
+import {FETCH_PROTECTED_DATA_REQUEST, RECEIVE_PROTECTED_DATA} from '../constants/ActionTypes';
+import {parseJSON} from '../utils/misc';
+import {get_user} from '../utils/api/user';
+import {logoutAndRedirect} from './auth';
+
+export function receiveProtectedData(data) {
+    return {
+        type: RECEIVE_PROTECTED_DATA,
+        payload: {
+            data,
+        },
+    };
+}
+
+export function fetchProtectedDataRequest() {
+    return {
+        type: FETCH_PROTECTED_DATA_REQUEST,
+    };
+}
+
+export function fetchProtectedData(token) {
+    return (dispatch) => {
+        dispatch(fetchProtectedDataRequest());
+
+        get_user(token)
+            .then(parseJSON)
+            .then(response => {
+                dispatch(receiveProtectedData(response.result));
+            })
+            .catch(error => {
+                if (error.status === 401) {
+                    dispatch(logoutAndRedirect(error));
+                }
+            });
+    };
+}
