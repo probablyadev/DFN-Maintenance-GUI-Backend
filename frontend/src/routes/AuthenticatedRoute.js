8be9a38b8f5732a12d88cbe21bd79bf7b232db7c
ScottDay <17160182@student.curtin.edu.au>
2018-03-09 13:06:38 +0800

    feat: route authentication

--- frontend/src/routes/AuthenticatedRoute.js
+++ frontend/src/routes/AuthenticatedRoute.js
@@ -4,24 +4,88 @@
 // If they are: they proceed to the page
 // If not: they are redirected to the login page.
 import React from 'react'
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import {Redirect, Route} from 'react-router-dom'
+import {isValidToken} from '../utils/misc';
+import * as actionCreators from '../actions/auth';
 
-const PrivateRoute = ({component: Component, ...rest}) => {
-    // const isLoggedIn = AuthService.isLoggedIn();
-    const isLoggedIn = false;
+function mapStateToProps(state) {
+    return {
+        token: state.auth.token,
+        userName: state.auth.userName,
+        isAuthenticated: state.auth.isAuthenticated,
+    };
+}
 
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                isLoggedIn ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
-                )
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class AuthenticatedRoute extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loadedIfNeeded: false
+        };
+    }
+
+    checkAuth() {
+        if (!this.props.isAuthenticated) {
+            const token = localStorage.getItem('token');
+
+            if (token) {
+                this.props.loginUserRequest();
+
+                isValidToken(JSON.stringify({token}))
+                    .then(response => {
+                        if (response.status === 200) {
+                            this.props.loginUserSuccess(token);
+
+                            this.setState({
+                                loadedIfNeeded: true,
+                            });
+                        } else {
+                            this.props.loginUserFailure({
+                                response: {
+                                    status: 403,
+                                    statusText: 'Invalid token'
+                                }
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        this.props.loginUserFailure({
+                            response: {
+                                status: 403,
+                                statusText: 'Failure while verifying user token',
+                            }
+                        });
+                    });
             }
-        />
-    );
-};
+        } else {
+            this.setState({
+                loadedIfNeeded: true,
+            });
+        }
+    }
 
-export default PrivateRoute;
+    render() {
+        this.checkAuth();
+
+        return (
+            <Route
+                {...this.props}
+                render={(props) =>
+                    this.state.loadedIfNeeded ? (
+                        <Component {...props} />
+                    ) : (
+                        <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
+                    )
+                }
+            />
+        );
+    }
+}
