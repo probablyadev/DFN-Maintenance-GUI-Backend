2b08c095611772d1efed1172418022beb7b711b4
ScottDay <17160182@student.curtin.edu.au>
2018-03-09 11:27:26 +0800

    fix: DEV_ENVIRONMENT to APP_SETTINGS

--- manage.py
+++ manage.py
@@ -19,10 +19,10 @@ def create_db():
 
 
 if __name__ == '__main__':
-    # Gets the DEV_ENVIRONMENT variable set within pycharms environment variables configuration script
+    # Gets the APP_SETTINGS variable set within pycharms environment variables configuration script
     # If True then this script is being run on a dev machine, if false then it's running on a camera
     # Defaults to false
-    environment = os.getenv('DEV_ENVIRONMENT', False)
+    environment = os.getenv('APP_SETTINGS', False)
 
     if environment is False:
         os.chdir("/opt/dfn-software/Desert-Fireball-Maintainence-GUI")
