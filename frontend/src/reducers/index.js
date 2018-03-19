6866c27e682c285e32cb6ae42ae734a1dd336578
ScottDay <17160182@student.curtin.edu.au>
2018-03-19 12:47:03 +0800

    feat: checkHDD selector and reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,20 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
+
+import createNetworkReducerWithType from "./network";
+import * as actionTypes from '../constants/ActionTypes';
 import settings from './settings';
 import auth from './auth';
+
+const hddReducer = combineReducers({
+    checkHDD: createNetworkReducerWithType(actionTypes.checkHDD)
+});
 
 const rootReducer = combineReducers({
     routing: routerReducer,
     settings,
-    auth
+    auth,
+    hddReducer
 });
 
 export default rootReducer;
---
+++ frontend/src/selectors/hdd.js
@@ -0,0 +1,1 @@
+const checkHDDSelector = state => state.checkHDD.data;
