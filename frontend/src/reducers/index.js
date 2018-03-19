05f354173a1a3fb3c7a3f82de40fbb3b43fad2bd
ScottDay <17160182@student.curtin.edu.au>
2018-03-19 12:30:12 +0800

    feat: removed data action / reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,13 +2,11 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 import settings from './settings';
 import auth from './auth';
-import data from './data';
 
 const rootReducer = combineReducers({
     routing: routerReducer,
     settings,
-    auth,
-    data
+    auth
 });
 
 export default rootReducer;
