0479b1e8c6236df47db6742a8df50d71e51daf67
ScottDay <17160182@student.curtin.edu.au>
2018-03-19 12:46:43 +0800

    feat: generic network reducer

---
+++ frontend/src/reducers/network.js
@@ -0,0 +1,28 @@
+import {createReducer} from '../utils/misc';
+
+const initialState = {
+    data: null,
+    loading: false,
+    error: null,
+};
+
+export default function createNetworkReducerWithType(type, configuredState = initialState) {
+    return createReducer(configuredState, {
+        [type.TRIGGER]: (state) =>
+            Object.assign({}, state, {
+                loading: true
+            }),
+        [type.SUCCESS]: (state, payload) =>
+            Object.assign({}, state, {
+                data: payload
+            }),
+        [type.FAILURE]: (state, payload) =>
+            Object.assign({}, state, {
+                error: payload
+            }),
+        [type.FULFILL]: (state) =>
+            Object.assign({}, state, {
+                loading: false
+            })
+    });
+}
