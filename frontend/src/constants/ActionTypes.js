d7a7e91e426acf90eac6da60a8b80a870ad34ad3
ScottDay <17160182@student.curtin.edu.au>
2018-03-20 12:08:09 +0800

    feat: API action types

--- frontend/src/constants/ActionTypes.js
+++ frontend/src/constants/ActionTypes.js
@@ -1,7 +1,60 @@
 import { createRoutine } from 'redux-saga-routines';
 
-// HDDAPISERVICE
+// Camera API Service
+export const cameraOff = createRoutine('CAMERA_OFF');
+export const cameraOn = createRoutine('CAMERA_ON');
+export const cameraStatus = createRoutine('CAMERA_STATUS');
+export const downloadPicture = createRoutine('DOWNLOAD_PICTURE');
+export const downloadThumbnail = createRoutine('DOWNLOAD_THUMBNAIL');
+export const findPictures = createRoutine('FIND_PICURES');
+export const removeThumbnail = createRoutine('REMOVE_THUMBNAIL');
+export const turnVideoCameraOff = createRoutine('TURN_VIDEO_CAMERA_OFF');
+export const turnVideoCameraOn = createRoutine('TURN_VIDEO_CAMERA_ON');
+
+// Config File API Service
+export const checkConfigFile = createRoutine('CHECK_CONFIG_FILE');
+export const configWhiteList = createRoutine('CONFIG_WHITELIST');
+export const configFile = createRoutine('CONFIG_FILE');
+export const updateConfigFile = createRoutine('UPDATE_CONFIG_FILE');
+
+// GPS API Service
+export const checkGPS = createRoutine('CHECK_GPS');
+
+// HDD API Service
 export const checkHDD = createRoutine('CHECK_HDD');
+export const enableHDD = createRoutine('ENABLE_HDD');
+export const formatHDD = createRoutine('FORMAT_HDD');
+export const mountHDD = createRoutine('MOUNT_HDD');
+export const moveData0HDD = createRoutine('MOVE_DATA_0_HDD');
+export const probeHDD = createRoutine('PROBE_HDD');
+export const smartTest = createRoutine('SMART_TEST');
+export const unmountHDD = createRoutine('UNMOUNT_HDD');
+
+// Interval Control Test API Service
+export const intervalTest = createRoutine('INTERVAL_TEST');
+export const prevIntervalTest = createRoutine('PREV_INTERVAL_TEST');
+
+// Misc API Service
+export const getHostname = createRoutine('GET_HOSTNAME');
+
+// Network API Service
+export const checkVPN = createRoutine('CHECK_VPN');
+export const checkInternet = createRoutine('CHECK_INTERNET');
+export const restartModem = createRoutine('RESTART_MODEM');
+export const restartVPN = createRoutine('RESTART_VPN');
+
+// Status API Service
+export const latestLog = createRoutine('LATEST_LOG');
+export const secondLatestLog = createRoutine('SECOND_LATEST_LOG');
+
+// Time API Service
+export const outputTime = createRoutine('OUTPUT_TIME');
+export const changeTimezone = createRoutine('CHANGE_TIMEZONE');
+
+// User API Service
+export const getUser = createRoutine('GET_USER');
+export const getToken = createRoutine('GET_TOKEN');
+export const isTokenValid = createRoutine('IS_TOKEN_VALID');
 
 export const TOGGLE_BOXED_LAYOUT = 'TOGGLE_BOXED_LAYOUT';
 export const TOGGLE_COLLAPSED_NAV = 'TOGGLE_COLLAPSED_NAV';
