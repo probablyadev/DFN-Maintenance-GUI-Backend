2ad174cf08ed046b597082d617fc273e74fa81ae
ScottDay <17160182@student.curtin.edu.au>
2018-03-20 12:08:19 +0800

    feat: API reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -6,15 +6,86 @@ import * as actionTypes from '../constants/ActionTypes';
 import settings from './settings';
 import auth from './auth';
 
+const cameraReducer = combineReducers({
+    cameraOff: createNetworkReducerWithType(actionTypes.cameraOff),
+    cameraOn: createNetworkReducerWithType(actionTypes.cameraOn),
+    cameraStatus: createNetworkReducerWithType(actionTypes.cameraStatus),
+    downloadPicture: createNetworkReducerWithType(actionTypes.downloadPicture),
+    downloadThumbnail: createNetworkReducerWithType(actionTypes.downloadThumbnail),
+    findPictures: createNetworkReducerWithType(actionTypes.findPictures),
+    removeThumbnail: createNetworkReducerWithType(actionTypes.removeThumbnail),
+    turnVideoCameraOff: createNetworkReducerWithType(actionTypes.turnVideoCameraOff),
+    turnVideoCameraOn: createNetworkReducerWithType(actionTypes.turnVideoCameraOn)
+});
+
+const configFileReducer = combineReducers({
+    checkConfigFile: createNetworkReducerWithType(actionTypes.checkConfigFile),
+    configWhiteList: createNetworkReducerWithType(actionTypes.configWhiteList),
+    configFile: createNetworkReducerWithType(actionTypes.configFile),
+    updateConfigFile: createNetworkReducerWithType(actionTypes.updateConfigFile)
+});
+
+const gpsReducer = combineReducers({
+    checkGPS: createNetworkReducerWithType(actionTypes.checkGPS)
+});
+
 const hddReducer = combineReducers({
-    checkHDD: createNetworkReducerWithType(actionTypes.checkHDD)
+    checkHDD: createNetworkReducerWithType(actionTypes.checkHDD),
+    enableHDD: createNetworkReducerWithType(actionTypes.enableHDD),
+    formatHDD: createNetworkReducerWithType(actionTypes.formatHDD),
+    mountHDD: createNetworkReducerWithType(actionTypes.mountHDD),
+    moveData0HDD: createNetworkReducerWithType(actionTypes.moveData0HDD),
+    probeHDD: createNetworkReducerWithType(actionTypes.probeHDD),
+    smartTest: createNetworkReducerWithType(actionTypes.smartTest),
+    unmountHDD: createNetworkReducerWithType(actionTypes.unmountHDD)
+});
+
+const intervalControlTestReducer = combineReducers({
+    intervalTest: createNetworkReducerWithType(actionTypes.intervalTest),
+    prevIntervalTest: createNetworkReducerWithType(actionTypes.prevIntervalTest)
+});
+
+const miscReducer = combineReducers({
+    getHostname: createNetworkReducerWithType(actionTypes.getHostname)
+});
+
+const networkReducer = combineReducers({
+    checkVPN: createNetworkReducerWithType(actionTypes.checkVPN),
+    checkInternet: createNetworkReducerWithType(actionTypes.checkInternet),
+    restartModem: createNetworkReducerWithType(actionTypes.restartModem),
+    restartVPN: createNetworkReducerWithType(actionTypes.restartVPN)
+});
+
+const statusReducer = combineReducers({
+    latestLog: createNetworkReducerWithType(actionTypes.latestLog),
+    secondLatestLog: createNetworkReducerWithType(actionTypes.secondLatestLog)
+});
+
+const timeReducer = combineReducers({
+    outputTime: createNetworkReducerWithType(actionTypes.outputTime),
+    changeTimezone: createNetworkReducerWithType(actionTypes.changeTimezone)
+});
+
+const userReducer = combineReducers({
+    getUser: createNetworkReducerWithType(actionTypes.getUser),
+    getToken: createNetworkReducerWithType(actionTypes.getToken),
+    isTokenValid: createNetworkReducerWithType(actionTypes.isTokenValid)
 });
 
 const rootReducer = combineReducers({
     routing: routerReducer,
     settings,
     auth,
-    hddReducer
+    cameraReducer,
+    configFileReducer,
+    gpsReducer,
+    hddReducer,
+    intervalControlTestReducer,
+    miscReducer,
+    networkReducer,
+    statusReducer,
+    timeReducer,
+    userReducer
 });
 
 export default rootReducer;
