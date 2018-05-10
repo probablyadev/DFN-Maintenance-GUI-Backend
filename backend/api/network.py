00d7f70d88aa225ce61c9d1cdea348e27658e2ad
ScottDay <17160182@student.curtin.edu.au>
2018-05-10 20:39:10 +0800

    feat: refactored network api

--- backend/api/network.py
+++ backend/api/network.py
@@ -10,11 +10,24 @@ network_endpoints = Blueprint("network_api", __name__)
 @requires_auth
 def check_vpn_endpoint():
     """Delivers a summary of the VPN connectivity of the system."""
-    check_vpn_message, check_vpn_status = check_vpn()
+    pingOutput, vpnIP = check_vpn()
 
     return jsonify(
-        check_vpn_status = check_vpn_status,
-        check_vpn_message = check_vpn_message
+        ping_output = pingOutput,
+        vpn_ip = vpnIP
+    )
+
+
+@network_endpoints.route("/api/network/restartVPN", methods = ["GET"])
+@requires_auth
+def restart_vpn_endpoint():
+    """Restarts the system's VPN daemon and checks for a vpn connection."""
+    restart_vpn()
+    pingOutput, vpnIP = check_vpn()
+
+    return jsonify(
+        ping_output = pingOutput,
+        vpn_ip = vpnIP
     )
 
 
@@ -22,11 +35,11 @@ def check_vpn_endpoint():
 @requires_auth
 def check_internet_endpoint():
     """Delivers a summary of the internet connectivity of the system."""
-    check_internet_message, check_internet_status = check_internet()
+    pingOutput, ipAddress = check_internet()
 
     return jsonify(
-        check_internet_status = check_internet_status,
-        check_internet_message = check_internet_message
+        ping_output = pingOutput,
+        ip_address = ipAddress
     )
 
 
@@ -34,25 +47,10 @@ def check_internet_endpoint():
 @requires_auth
 def restart_modem_endpoint():
     """Restarts the modem network interface."""
-    restart_modem_message = restart_modem()
-    check_internet_message, check_internet_status = check_internet()
+    restart_modem()
+    pingOutput, ipAddress = check_internet()
 
     return jsonify(
-        check_internet_status = check_internet_status,
-        restart_modem_message = restart_modem_message,
-        check_internet_message = check_internet_message
-    )
-
-
-@network_endpoints.route("/api/network/restartVPN", methods = ["GET"])
-@requires_auth
-def restart_vpn_endpoint():
-    """Restarts the system's VPN daemon."""
-    restart_vpn_message = restart_vpn()
-    check_vpn_message, check_vpn_status = check_vpn()
-
-    return jsonify(
-        check_vpn_status = check_vpn_status,
-        restart_vpn_message = restart_vpn_message,
-        check_vpn_message = check_vpn_message
+        ping_output = pingOutput,
+        ip_address = ipAddress
     )
