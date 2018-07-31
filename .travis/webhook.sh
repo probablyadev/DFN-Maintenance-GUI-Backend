aa43ce44f17f33d3e90f4ec788bc6e067bb86f60
ScottDay <17160182@student.curtin.edu.au>
2018-07-31 18:00:11 +1000

    chore(travis): fix build scripts

--- .travis/webhook.sh
+++ .travis/webhook.sh
@@ -1,5 +1,4 @@
-curl -s -X POST -H "Content-Type: application/json" \
-	-H "Accept: application/json" -H "Travis-API-Version: 3" \
-	-H "Authorization: token ${TRAVIS_PRIVATE_REPO_TOKEN}" \
-	-d '{"request":{"branch":"master"}}' \
+curl -s -X POST -H Content-Type: application/json \
+	-H Accept: application/json -H Travis-API-Version: 3 \
+	-H Authorization: token ${TRAVIS_PRIVATE_REPO_TOKEN} \
 	https://api.travis-ci.com/repo/ScottDay/DFN-Maintenance-GUI/requests
