c775fa3a200a374450148a7f8997d4d814e6b196
ScottDay <17160182@student.curtin.edu.au>
2018-08-21 12:26:37 +1000

    feat(config): move config to config folder and remove prod config

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ __version__ = '0.0.0'
 
 from sys import argv
 from flask import send_from_directory
-from flask.helpers import get_debug_flag
 
 from src.app import create_app
-from src.settings import DevelopmentConfig, ProductionConfig
 
 
 # DEVELOPMENT: pip3 install -r requirements.txt; python3 main.py dev
@@ -19,24 +17,34 @@ if len(argv) > 0:
 	env = argv[1]
 
 	if env == "dev":
+		from config.dev_config import DevelopmentConfig
+
 		config = DevelopmentConfig
 	elif env == "prod":
+		from config.dev_config import ProductionConfig
+
 		config = ProductionConfig
 else:
+	from config.dev_config import ProductionConfig
+
 	config = ProductionConfig
 
 app = create_app(config)
+
 
 @app.route('/')
 def index():
 	return send_from_directory('../dist', 'index.html')
 
+
 @app.route('/<filename>')
 def page(filename):
 	return send_from_directory('../dist', 'index.html')
 
+
 @app.route('/assets/<filename>')
 def assets(filename):
 	return send_from_directory('../dist/assets', filename)
+
 
 app.run(host = '0.0.0.0', port = 5000)
