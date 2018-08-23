f174574ff2b87551923e6aa950168898451782fb
ScottDay <17160182@student.curtin.edu.au>
2018-08-23 12:06:49 +1000

    chore(script): shortcut dev startup script

---
+++ start-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prepare for script execution.
+# pip3 install -r requirements/dev.txt
+# python3 db/create_db.py
+
+# Starts the program in the dev configuration.
+python3 main.py dev
