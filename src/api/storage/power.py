506f04eed64d136543d3a6da23369e431abd9f90
Scott Day <17160182@student.curtin.edu.au>
2018-09-12 11:54:44 +1000

    fix(python): storage uindent level to tabs

--- src/api/storage/power.py
+++ src/api/storage/power.py
@@ -23,10 +23,10 @@ def on():
 	# Give drives time to register.
 	sleep(25)
 
-    # For EXT, re-scan SATA/SCSI hotswap drives.
-    if 'EXT' in hostname():
-        console("for i in $(find /sys/class/scsi_host/ -name host* ); do echo '- - -' > $i/scan")
-        sleep(2)
+	# For EXT, re-scan SATA/SCSI hotswap drives.
+	if 'EXT' in hostname():
+		console("for i in $(find /sys/class/scsi_host/ -name host* ); do echo '- - -' > $i/scan")
+		sleep(2)
 
 	return jsonify(partitions = mount()), 200
 
@@ -38,9 +38,9 @@ def off():
 
 	ext = True if 'EXT' in hostname() else False
 
-    # For EXT, delete the devices ONLY if they're all not solid state devices.
-    if ext:
-    	# Used for deleting devices in EXTs before powering off.
+	# For EXT, delete the devices ONLY if they're all not solid state devices.
+	if ext:
+		# Used for deleting devices in EXTs before powering off.
 		drives_to_check = current_app.config['DRIVES_TO_CHECK']
 
 		for drive in drives_to_check:
@@ -51,17 +51,17 @@ def off():
 				if not search('[0-9]', rotation):
 					raise RuntimeError('External drive {0} is listed as an SSD, while it should be a HDD. Use the command line to resolve this.')
 
-        for drive in drives_to_check:
+		for drive in drives_to_check:
 			device = sub('/dev/', '', drive['device'])
-            console('echo 1 > /sys/block/{0}/device/delete'.format(device))
+			console('echo 1 > /sys/block/{0}/device/delete'.format(device))
 
-        sleep(1)
+		sleep(1)
 
 	# Power off.
 	console('python /opt/dfn-software/disable_ext-hd.py')
 
-    # Sleep if EXT, needs time to remove drives.
-    if ext:
-        time.sleep(22)
+	# Sleep if EXT, needs time to remove drives.
+	if ext:
+		time.sleep(22)
 
 	return jsonify(partitions = check()), 200
