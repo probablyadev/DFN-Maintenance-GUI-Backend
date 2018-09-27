d1cdcc0f432ceda6e3a3087d105d6b56fa3e467b
Scott Day <17160182@student.curtin.edu.au>
2018-09-27 13:26:12 +1000

    refactor(location): update to new endpoint handler

--- src/api/location/gps.py
+++ src/api/location/gps.py
@@ -1,8 +1,6 @@
 """The location gps api module /location/gps endpoints."""
 
-from flask import jsonify
-
-from src.wrappers import old_endpoint, jwt
+from src.wrappers import jwt, endpoint, current_app_injecter
 from src.console import console
 
 
@@ -17,12 +15,15 @@ def coordinates(initial, direction):
 
 
 @jwt
-@old_endpoint()
-def get():
+@endpoint()
+@current_app_injecter()
+def get(handler, log):
+	log.info('Getting GPS status.')
 	output = console(
 		'python /opt/dfn-software/leostick_get_status.py -g',
 		'echo GPGGA,080112.000,3346.4614,S,15106.8787,E,0,00,99.0,067.59,M,21.9,M,,,').split(',')
 
+	log.info('Parsing GPS output.')
 	if len(output) is not 16:
 		raise IOError('GPS offline')
 
@@ -32,10 +33,10 @@ def get():
 	longitude = coordinates(output[4], output[5])
 	altitude = output[9].strip('0') or '0'
 
-	return jsonify(
+	handler.add_to_response(
 		lock = lock,
 		satellites = satellites,
 		latitude = latitude,
 		longitude = longitude,
 		altitude = altitude
-	), 200
+	)
--- src/api/location/time.py
+++ src/api/location/time.py
@@ -1,9 +1,8 @@
 """The location time api module /location/time endpoints."""
 
-from flask import jsonify
 from re import sub
 
-from src.wrappers import old_endpoint, jwt
+from src.wrappers import jwt, endpoint, current_app_injecter, log_doc
 from src.console import console
 
 
@@ -11,21 +10,30 @@ __all__ = ['get', 'put']
 
 
 @jwt
-@old_endpoint()
-def get():
+@endpoint()
+def get(handler, log):
+	log.info('Getting time status.')
 	time = console('timedatectl status').splitlines()
 
+	log.info('Parsing time.')
 	local = time[0].split('Local time: ')[1]
 	utc = time[1].split('Universal time: ')[1]
 	utc = sub('UTC', '', utc)
 	rtc = time[2].split('RTC time: ')[1]
 	timezone = time[3].split('Time zone: ')[1].split(' ')[0]
 
-	return jsonify(local = local, utc = utc, rtc = rtc, timezone = timezone), 200
+	handler.add_to_response(
+		local = local,
+		utc = utc,
+		rtc = rtc,
+		timezone = timezone
+	)
 
 
 @jwt
-@old_endpoint()
-def put(timezone):
+@log_doc('Setting new timezone.')
+@endpoint()
+@current_app_injecter()
+def put(timezone, handler):
 	console("timedatectl set-timezone {0}".format(timezone[0]))
-	return 204
+	handler.set_status(204)
