00628ffb9216b5f809a0194aa0d612807de33398
Scott Day <17160182@student.curtin.edu.au>
2018-09-27 13:51:18 +1000

    refactor(network): update to new endpoint handler

--- src/api/network/internet.py
+++ src/api/network/internet.py
@@ -1,15 +1,16 @@
 """The network api module /internet endpoints."""
 
-from flask import jsonify
 from subprocess import CalledProcessError
 
 from src.console import console
-from src.wrappers import old_endpoint, jwt
+from src.wrappers import jwt, endpoint, current_app_injecter, log_doc
 
 
 @jwt
-@old_endpoint()
-def check():
+@endpoint()
+@current_app_injecter()
+def check(handler, log):
+	log.info('Getting IP address.')
 	#ip = console("dig TXT +short o-o.myaddr.l.google.com @ns1.google.com").replace('"', '')
 	command = "ifconfig | grep eth1 -A 1 | grep -o '\(addr:\|inet \)[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | cut -c6-"
 	ip = console(command)
@@ -17,11 +18,17 @@ def check():
 	if len(ip) == 0:
 		raise CalledProcessError(cmd = command, returncode = 1, output = "Unable to find IP address")
 
+	log.info('Checking internet connectivity.')
 	output = console("ping -c 1 www.google.com")
 
-	return jsonify(ip = ip, output = output), 200
+	handler.add_to_response(
+		ip = ip,
+		output = output
+	)
 
 @jwt
-@old_endpoint()
-def restart():
-	return jsonify(output = console("ifdown ppp0 && sleep 8 && ifup ppp0 && sleep 8 && ifconfig ppp0")), 200
+@log_doc('Restarting internet adapter.')
+@endpoint()
+@current_app_injecter()
+def restart(handler):
+	handler.add_to_response(console("ifdown ppp0 && sleep 8 && ifup ppp0 && sleep 8 && ifconfig ppp0"))
--- src/api/network/vpn.py
+++ src/api/network/vpn.py
@@ -1,26 +1,34 @@
 """The network api module /vpn endpoints."""
 
-from flask import jsonify
 from subprocess import CalledProcessError
 
 from src.console import console
-from src.wrappers import old_endpoint, jwt
+from src.wrappers import jwt, endpoint, current_app_injecter, log_doc
 
 
 @jwt
-@old_endpoint()
-def check():
+@endpoint()
+@current_app_injecter()
+def check(handler, log):
+	log.info('Getting VPN address.')
 	command = "ifconfig | grep tun0 -A 1 | grep -o '\(addr:\|inet \)[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'| cut -c6-"
 	ip = console(command)
 
 	if len(ip) == 0:
 		raise CalledProcessError(cmd = command, returncode = 1, output = "Unable to find VPN IP address")
 
+	log.info('Checking VPN connectivity.')
 	output = console("ping -c 1 10.1.16.1")
 
-	return jsonify(ip = ip, output = output), 200
+	handler.add_to_response(
+		ip = ip,
+		output = output
+	)
+
 
 @jwt
-@old_endpoint()
-def restart():
-	return jsonify(output = console("service openvpn restart && sleep 10 && ifconfig tun0")), 200
+@log_doc('Restarting VPN adapter.')
+@endpoint()
+@current_app_injecter()
+def restart(handler):
+	handler.add_to_response(console("service openvpn restart && sleep 10 && ifconfig tun0"))
