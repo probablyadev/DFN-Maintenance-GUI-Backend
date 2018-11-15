63871aabb6826e2d69de61ff892f0265a0d0ebda
Scott Day <17160182@student.curtin.edu.au>
2018-11-15 15:12:30 +1100

    refactor(power): remove unused decorators

--- src/api/power/power.py
+++ src/api/power/power.py
@@ -4,17 +4,13 @@ from src.console import console
 
 @wrappers.jwt
 @wrappers.endpoint
-@wrappers.stats
 @wrappers.logger('Shutting down the server.')
-@wrappers.injector
 def shutdown():
 	console('shutdown --poweroff now')
 
 
 @wrappers.jwt
 @wrappers.endpoint
-@wrappers.stats
 @wrappers.logger('Restarting the server.')
-@wrappers.injector
 def restart():
 	console('shutdown --reboot now')
