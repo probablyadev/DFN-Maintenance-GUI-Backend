5d6552b90c004d86d77395b350aaef2d41fb333d
Scott Day <17160182@student.curtin.edu.au>
2018-11-15 16:13:04 +1100

    refactor(config): remove old config handler

--- src/setup/__init__.py
+++ src/setup/__init__.py
@@ -1,8 +1,7 @@
-from .config import config
 from .args import args
 from .extensions import extensions
 from .logger import logger
 from .routes import routes
 
 
-__all__ = ['config', 'args', 'extensions', 'logger', 'routes']
+__all__ = ['args', 'extensions', 'logger', 'routes']
